bc5ca2158ac098032662b5381afc795d281e05b3
sdaliya14-cell <sdaliya14@gmail.com>
2026-02-20 11:40:42 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+from sklearn.datasets import load_iris
+data=load_iris()
+from sklearn.ensemble import RandomForestRegressor
+model=RandomForestClassifier()
+x=data.data
+y=data.target
+model.fit(x,y)
+st.header("Iris flower Classification")
+sl=st.number_input('enter sepal length')
+sw=st.number_input('enter sepal width')
+pl=st.number_input('enter sepal length')
+pw=st.number_input('enter sepal width')
+y_pred=model.predict([[sl,sw,pi,pw]])
+op=data.target_names[y_pred[0]]
+st.write(op)
